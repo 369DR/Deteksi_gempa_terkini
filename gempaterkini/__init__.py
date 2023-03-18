1953af603a3f4e2c7d8850aaaaa12bbe3e55fd5a
DR <dapatheai@gmail.com>
2023-03-18 21:04:29 +0700

    Menggunakan 2 package yaitu beautifulsoup4 dan requests dengan memperbaiki kesalahan menggunakan (try dan except Exception)

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,3 +1,8 @@
+import requests
+import bs4
+
+
+
 def ekstrasi_data():
     """
     Tanggal     : 18 Maret 2023,
@@ -9,17 +14,30 @@ def ekstrasi_data():
     keterangan  : tidak berpotensi TSUNAMI
     :return:
     """
-    hasil = dict ()
-    hasil["tanggal"] = "18 Maret 2023"
-    hasil["waktu"] = "02:38:00 WIB"
-    hasil["magnitudo"] = 5.1
-    hasil["kedalaman"] = "91 km"
-    hasil["lokasi"] = {'ls':5.90, 'bt':125.99}
-    hasil["pusat gempa"] = "224 km BaratLaut MELONGUANE-SULUT"
-    hasil["keterangan"] = "tidak berpotensi TSUNAMI"
+    try:
+        content = requests.get("https://bmkg.go.id")
+    except Exception:
+        return None
+    if content.status_code == 200:
+        print(content.text)
+        # soup = bs4.BeautifulSoup(content)
+        # print(soup.prettify())
 
-    return  hasil
+        hasil = dict ()
+        hasil["tanggal"] = "18 Maret 2023"
+        hasil["waktu"] = "02:38:00 WIB"
+        hasil["magnitudo"] = 5.1
+        hasil["kedalaman"] = "91 km"
+        hasil["lokasi"] = {'ls':5.90, 'bt':125.99}
+        hasil["pusat gempa"] = "224 km BaratLaut MELONGUANE-SULUT"
+        hasil["keterangan"] = "tidak berpotensi TSUNAMI"
+        return  hasil
+    else:
+        return  None
 def tampilkan_data(result):
+    if result is None:
+        print("Tidak bisa menemukan data gempa terkini")
+        return
     print("Gempa Terakhir beradasarkan BMKG ")
     print(f"Tanggal,{result['tanggal']}")
     print(f"waktu,{result['waktu']}")
@@ -29,5 +47,6 @@ def tampilkan_data(result):
     print(f"pusat gempa,{result['pusat gempa']}")
     print(f"keterangan,{result['keterangan']}")
 
-# if __name__ == '__main__':
-#     print("ini adalah pckage gempa terkini")
+if __name__ == '__main__':
+    print("ini adalah pckage gempa terkini")
+    print("Hai")
