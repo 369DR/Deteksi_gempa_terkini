7f0b8bcd661befdcb5da4554a6424290f2888693
DR <dapatheai@gmail.com>
2023-03-20 10:45:13 +0700

    Install package Newestearthquake-Indonesia-BMKG

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ Aplikasi deteksi gempa terkini
 MODULARISASI dengan FUNGTION
 MODULARISASI dengan PACKAGE
 """
-import  gempaterkini
+import  gempaterbaru
 
 if __name__ == '__main__':
     print("Aplikasi utama")
-    result = gempaterkini.ekstrasi_data()
-    gempaterkini.tampilkan_data(result)
+    result = gempaterbaru.ekstrasi_data()
+    gempaterbaru.tampilkan_data(result)
